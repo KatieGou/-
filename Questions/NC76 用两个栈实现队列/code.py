b4d0cee1d773376d7b32e45bcfabd087aa1ba0f3
Katie <71438233+KatieGou@users.noreply.github.com>
2021-08-20 17:29:54 +0300

    Create code.py

---
+++ Questions/NC76 用两个栈实现队列/code.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+    def push(self, node):
+        # write code here
+        self.stack1.append(node)
+    def pop(self):
+        # return xx
+        while self.stack1:
+            self.stack2.append(self.stack1.pop(0))
+        return self.stack2.pop(0)
